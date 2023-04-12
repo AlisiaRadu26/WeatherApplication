a0e24dc1bbfbc056f5bfd675ec08b8f977435f3c
AlisiaRadu26 <117165690+AlisiaRadu26@users.noreply.github.com>
2023-04-12 09:53:55 +0300

    Update ForecastWeek.jsx

--- src/components/ForecastWeek.jsx
+++ src/components/ForecastWeek.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
+
 import "../styles/style_forecast.css";
 import "../styles/style_day_forecast.css";
 import "../media_screen/media_forecast.css";
